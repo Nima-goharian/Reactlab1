149c9b42f5de973de3fc1e32b2f4e5b325e2762b
Nima Goharian <ngoharian144@gmail.com>
2024-09-17 18:11:08 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-function App() { 
-  return ( 
-    <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
-      <Heading /> 
-    </div> 
-  ); 
-} 
- 
+function Heading() {
+  return <h1>This is a heading</h1>;
+}
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
 export default App;
